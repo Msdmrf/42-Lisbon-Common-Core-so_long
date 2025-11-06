1f7d42763940a1a848947aa902bc4509bd927d9e
Miguel Ramirez <miguel.matos.fernandes1@gmail.com>
2025-11-06 16:16:50 +0000

    Change so_long header

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: migusant <migusant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 16:19:09 by migusant          #+#    #+#             */
-/*   Updated: 2025/08/04 19:22:52 by migusant         ###   ########.fr       */
+/*   Updated: 2025/11/05 18:58:36 by migusant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,11 @@
 #  define MAX_MAP_HEIGHT 33
 # endif
 
-// Key Codes
+// Keysyms
 # define KEY_ESC 65307
+# define KEY_S 115
 # define KEY_W 119
 # define KEY_A 97
-# define KEY_S 115
 # define KEY_D 100
 
 // Game Elements
@@ -61,7 +61,7 @@
 # define PLAYER_ANIMATIONS 8
 # define PLAYER_IDLE_DELAY 60000
 
-// Animation States
+// Player Animation
 # define ANIM_IDLE_DOWN 0
 # define ANIM_IDLE_UP 1
 # define ANIM_IDLE_LEFT 2
@@ -71,14 +71,21 @@
 # define ANIM_WALK_LEFT 6
 # define ANIM_WALK_RIGHT 7
 
+// Enemies Animation
+# define ENEMY_ANIM_IDLE_DOWN 0
+# define ENEMY_ANIM_WALK_DOWN 1
+# define ENEMY_ANIM_WALK_UP 2
+# define ENEMY_ANIM_WALK_LEFT 3
+# define ENEMY_ANIM_WALK_RIGHT 4
+
 // Player States
-# define PLAYER_STATE_IDLE 0
-# define PLAYER_STATE_STATIC 1
-# define PLAYER_STATE_MOVING 2
+# define PLAYER_STATE_STATIC 0
+# define PLAYER_STATE_MOVING 1
+# define PLAYER_STATE_IDLE 2
 
 // Enemy States
-# define ENEMY_STATE_MOVING 0
-# define ENEMY_STATE_STATIC 1
+# define ENEMY_STATE_STATIC 0
+# define ENEMY_STATE_MOVING 1
 
 // Static Direction Constants
 # define DIRECTION_DOWN 0
@@ -100,9 +107,9 @@ typedef struct s_collectible
 {
 	int		count;
 	int		collected;
+	int		curr_frame;
+	int		anim_timer;
 	void	*sprites[ANIMATION_FRAMES];
-	int		frame;
-	int		timer;
 }		t_collectible;
 
 typedef struct s_player
@@ -110,13 +117,13 @@ typedef struct s_player
 	int		x;
 	int		y;
 	int		moves;
-	int		timer;
+	int		state;
+	int		curr_anim;
+	int		curr_frame;
+	int		last_direction;
+	int		move_timer;
 	int		anim_timer;
 	int		idle_timer;
-	int		current_anim;
-	int		last_direction;
-	int		frame;
-	int		state;
 	void	*sprites[PLAYER_ANIMATIONS][ANIMATION_FRAMES];
 	void	*static_sprites[DIRECTION_COUNT];
 	void	*exit_sprites[PLAYER_ANIMATIONS][ANIMATION_FRAMES];
@@ -125,28 +132,26 @@ typedef struct s_player
 
 typedef struct s_enemy
 {
-	int		start_x;
-	int		start_y;
 	int		x;
 	int		y;
 	char	type;
-	int		direction;
-	int		current_anim;
-	int		frame;
 	int		state;
+	int		direction;
+	int		curr_anim;
+	int		curr_frame;
 	int		static_frame;
 	int		static_timer;
 }		t_enemy;
 
-typedef struct s_enemy_patrol
+typedef struct s_enemy_manager
 {
-	t_enemy	*patrol;
+	t_enemy	*enemy;
 	int		count;
-	int		timer;
+	int		move_timer;
 	int		anim_timer;
 	void	*sprites[ENEMY_ANIMATIONS][ANIMATION_FRAMES];
 	void	*static_sprites[DIRECTION_COUNT];
-}		t_enemy_patrol;
+}		t_enemy_manager;
 
 typedef struct s_game
 {
@@ -163,114 +168,99 @@ typedef struct s_game
 	t_keys			keys;
 	t_player		player;
 	t_collectible	collectible;
-	t_enemy_patrol	enemy;
+	t_enemy_manager	enemies;
 	int				exit_x;
 	int				exit_y;
 }		t_game;
 
-// Game Lifecycle (main.c)
-int		init_window(t_game *game);
-void	free_map(char **map);
-int		close_game(t_game *game);
-
-// Map Loading (map_loading.c)
+// Map Loading (map_loading.c) (4 / 4)
 int		load_map(t_game *game, char *filename);
-int		count_lines(char *filename);
-char	**allocate_map(int lines);
-int		read_map_lines(t_game *game, char *filename);
 
-// Map Validation Core (map_validation_core.c)
+// Map Validation Core (map_validation_core.c) (5 / 5)
 int		validate_map(t_game *game, char *filename);
-int		check_file_extension(char *filename);
-int		check_rectangular(t_game *game);
-int		check_map_size(t_game *game);
-int		check_invalid_chars(t_game *game);
 
-// Map Validation Elements (map_validation_elements.c)
+// Map Validation Elements (map_validation_elements.c) (5 / 5)
 int		check_walls(t_game *game);
 int		check_player(t_game *game);
 int		check_collectibles(t_game *game);
 int		check_enemies(t_game *game);
 int		check_exit(t_game *game);
 
-// Map Pathfinding (map_pathfinding.c)
+// Map Pathfinding (map_pathfinding.c) (4 / 4)
 int		check_path(t_game *game);
-char	**copy_map(t_game *game);
-int		flood_fill(char **map, int x, int y, t_game *game);
-int		is_reachable(t_game *game, char **map_copy);
 
-// Sprite Loading (sprites_manager.c)
+// Game Lifecycle (main.c) (1 / 3)
+int		init_window(t_game *game);
+
+// Sprite Loading (sprites_manager.c) (3 / 5)
 int		load_sprites(t_game *game);
-int		load_basic_sprites(t_game *game);
-int		load_animation_sprites(t_game *game);
-void	destroy_images(t_game *game);
-void	destroy_sprite_arrays(t_game *game);
 
-// Collectible Sprites (sprites_collectible.c)
-int		load_collectible_sprites(t_game *game);
-
-// Player Sprites (sprites_player.c)
+// Player Sprites (sprites_player.c) (5 / 5)
 int		load_player_sprites(t_game *game);
 
-// Enemy Sprites (sprites_enemy.c)
+// Collectible Sprites (sprites_collectible.c) (1 / 1)
+int		load_collectible_sprites(t_game *game);
+
+// Enemy Sprites (sprites_enemy.c) (3 / 3)
 int		load_enemy_sprites(t_game *game);
 
-// Map Rendering (render_map.c)
-void	render_map(t_game *game);
-void	render_tile(t_game *game, int x, int y, void *img);
-void	render_tiles(t_game *game, int x, int y);
+// Enemy System (enemy_system.c) (2 / 5)
+void	init_enemies(t_game *game);
 
-// Entity Rendering (render_entities.c)
-void	render_collectible_frame(t_game *game, int x, int y);
-void	render_player_frame(t_game *game, int x, int y);
-void	render_enemy_frame(t_game *game, int x, int y, t_enemy *enemy);
-
-// UI Rendering (render_ui.c)
-void	display_move_counter(t_game *game);
-void	render_move_text(t_game *game, char *moves_str);
-
-// Animation Updates (update_animations.c)
-void	update_collectible_sprites(t_game *game);
-void	update_player_sprites(t_game *game);
-void	update_enemy_sprites(t_game *game);
-
-// Player System (player_system.c)
-void	init_player_animation(t_game *game);
-void	set_player_static(t_game *game);
-void	set_player_animation(t_game *game, int direction);
-int		handle_keypress(int keycode, t_game *game);
-int		handle_keyrelease(int keycode, t_game *game);
-
-// Player Actions (player_actions.c)
-void	move_player(t_game *game, int new_x, int new_y);
-void	handle_movement(t_game *game, int *alternate);
-
-// Input Handling (player_diagonal.c)
-void	handle_dual_keys(t_game *game, int *alternate);
-
-// Enemy System (enemy_system.c)
-void	init_enemy(t_enemy *enemy, int x, int y, char type);
-void	init_enemy_patrol(t_game *game);
-void	update_enemy_states(t_game *game);
-void	set_enemy_static_frame(t_enemy *enemy);
-void	set_enemy_animation(t_enemy *enemy);
-
-// Enemy Actions (enemy_actions.c)
-void	update_enemy(t_game *game);
-void	move_enemy(t_game *game, t_enemy *enemy);
-void	calculate_new_position(t_enemy *enemy, int *new_x, int *new_y);
-void	update_enemy_position(t_game *game, t_enemy *enemy,
-			int new_x, int new_y);
-int		is_valid_enemy_move(t_game *game, int x, int y, t_enemy *moving_enemy);
-
-// Game Loop (game_loop.c)
+// Game Loop (game_loop.c) (1 / 1)
 int		game_loop(t_game *game);
 
-// Game Interactions (game_interactions.c)
-void	handle_collectible(t_game *game, int new_x, int new_y);
-void	handle_enemy_collision(t_game *game);
-void	handle_exit(t_game *game);
+// Enemy Actions (enemy_actions.c) (5 / 5)
+void	update_enemy(t_game *game);
+
+// Enemy System (enemy_system.c) (5 / 5)
+void	update_enemy_states(t_game *game);
+void	set_enemy_animation(t_enemy *enemy);
+void	set_enemy_static_frame(t_enemy *enemy);
+
+// Player System (player_system.c) (2 / 4)
+int		handle_keypress(int keysym, t_game *game);
+int		handle_keyrelease(int keysym, t_game *game);
+
+// Input Handling (player_diagonal.c) (5 / 5)
+void	handle_dual_keys(t_game *game, int *alternate);
+
+// Player Actions (player_actions.c) (5 / 5)
+void	handle_movement(t_game *game, int *alternate);
+void	move_player(t_game *game, int new_x, int new_y);
+
+// Player System (player_system.c) (4 / 4)
+void	set_player_static(t_game *game);
+void	set_player_animation(t_game *game, int direction);
+
+// Game Interactions (game_interactions.c) (5 / 5)
 void	handle_tiles(t_game *game, int new_x, int new_y);
-void	unlock_exit(t_game *game);
+void	handle_enemy_collision(t_game *game);
+
+// Animation Updates (update_animations.c) (5 / 5)
+void	update_collectible_sprites(t_game *game);
+void	update_enemy_sprites(t_game *game);
+void	update_player_sprites(t_game *game);
+
+// Map Rendering (render_map.c) (3 / 3)
+void	render_map(t_game *game);
+void	render_tiles(t_game *game, int x, int y);
+
+// Entity Rendering (render_entities.c) (5 / 5)
+void	render_enemy_frame(t_game *game, t_enemy *enemy, int x, int y);
+void	render_collectible_frame(t_game *game, int x, int y);
+void	render_player_frame(t_game *game, int x, int y);
+
+// UI Rendering (render_ui.c) (2 / 2)
+void	display_move_counter(t_game *game);
+
+// Game Lifecycle (main.c) (2 / 3)
+int		close_game(t_game *game);
+
+// Sprite Loading (sprites_manager.c) (5 / 5)
+void	destroy_images(t_game *game);
+
+// Map Loading (main.c) (3 / 3)
+void	free_map(char **map);
 
 #endif
